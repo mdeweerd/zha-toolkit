ebca6b5bd22dc6c4e4a39430483209ce2b2a597e
mdeweerd <mdeweerd@users.noreply.github.com>
2022-01-21 23:50:21 +0100

    Flow setup

--- .github/scripts/update_hacs_manifest.py
+++ .github/scripts/update_hacs_manifest.py
@@ -1,20 +1,25 @@
+#!/bin/env python3
+#
+# Takes --version X.Y.Z or -V X.Y.Z option and sets version in manifest.json.
+# Must be launched from the root of the repository.
+#
 # Modified from : https://raw.githubusercontent.com/bramstroker/homeassistant-zha-toolkit/master/.github/scripts/update_hacs_manifest.py
-# 
+#
 # MIT License
-# 
+#
 # Copyright (c) 2021 Bram Gerritsen
 # Copyright (c) 2022 Mario DE WEERD
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,17 +42,16 @@ def update_manifest():
             version = sys.argv[index + 1]
 
     with open(
-        f"{os.getcwd()}/custom_components/zha-toolkit/manifest.json"
+        f"{os.getcwd()}/custom_components/zha_toolkit/manifest.json"
     ) as manifestfile:
         manifest = json.load(manifestfile)
 
     manifest["version"] = version
 
     with open(
-        f"{os.getcwd()}/custom_components/zha-toolkit/manifest.json", "w"
+        f"{os.getcwd()}/custom_components/zha_toolkit/manifest.json", "w"
     ) as manifestfile:
         manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))
 
 
 update_manifest()
-
