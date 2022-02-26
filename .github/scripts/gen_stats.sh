3975ff979fdbffa2a5221247895c37499f8b4008
mdeweerd <mdeweerd@users.noreply.github.com>
2022-02-26 12:20:39 +0100

    Exclude versions from badge generation

--- .github/scripts/gen_stats.sh
+++ .github/scripts/gen_stats.sh
@@ -4,10 +4,15 @@ DEST=$(dirname $0)/../../STATS.md
 
 TEMPLATE='- (![badge VERSION](https://img.shields.io/github/downloads/mdeweerd/zha-toolkit/VERSION/total.svg)'
 
+# Exclude stuff that results in invalid badges
+EXCLUDES="v0.7.9 v0.7.7 v0.7.6 v0.7.5 v0.7.3 v0.7.2 v0.7.1"
+
 (
-echo '# Badges showing number of downloads per version'
-echo
-for tag in latest $(git tag -l  --sort=-creatordate v*[0-9]) ; do
-   echo ${TEMPLATE//VERSION/$tag}
-done
+    echo '# Badges showing number of downloads per version'
+    echo
+    for tag in latest $(git tag -l  --sort=-creatordate v*[0-9]) ; do
+       if [[ "$EXCLUDES" != *"$tag"* ]] ; then
+           echo ${TEMPLATE//VERSION/$tag}
+       fi
+    done
 ) > "${DEST}"
